bae3f1c701545ad1e7f83cdf7adede15dd49be68
elleshar2052 <40801362+elleshar2052@users.noreply.github.com>
2018-10-24 19:45:18 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private int money;
+        private List<Product> products;
+
+        public Person(string name, int money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.Products = new List<Product>();
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+        
+        public int Money
+        {
+            get { return money; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+        
+        public List<Product> Products
+        {
+            get { return products; }
+            set { products = value; }
+        }
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+
+        public List<Product> SeeBag()
+        {
+            return Products;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] peopleInput = Console.ReadLine().Split(new[] {'=', ';'}, StringSplitOptions.RemoveEmptyEntries);
+            string[] productsInput = Console.ReadLine().Split(new[] {'=', ';'}, StringSplitOptions.RemoveEmptyEntries);
+            List<Person> people = new List<Person>();
+            List<Product> products = new List<Product>();
+            for (int i = 0; i < peopleInput.Length; i += 2)
+            {
+                string name = peopleInput[i];
+                int money = int.Parse(peopleInput[i + 1]);
+                try
+                {
+                    people.Add(new Person(name, money));
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+
+            for (int i = 0; i < productsInput.Length; i += 2)
+            {
+                string name = productsInput[i];
+                int cost = int.Parse(productsInput[i + 1]);
+                try
+                {
+                    products.Add(new Product(name, cost));
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split();
+                if (command[0] == "END")
+                {
+                    break;
+                }
+
+                string name = command[0];
+                string productName = command[1];
+                Person person = people.Where(n => n.Name == name).First();
+                Product product = products.Where(n => n.Name == productName).First();
+                if (person.Money < product.Cost)
+                {
+                    Console.WriteLine($"{name} can't afford {productName}");
+                }
+                else
+                {
+                    person.Money -= product.Cost;
+                    person.AddProduct(product);
+                    Console.WriteLine($"{name} bought {productName}");
+                }
+            }
+
+            foreach (var person in people)
+            {
+                if (person.Products.Count <= 0)
+                {
+                    Console.WriteLine($"{person.Name} - Nothing bought");
+                }
+                else
+                {
+                    Console.WriteLine($"{person.Name} - {string.Join(", ", person.SeeBag().Select(k => k.Name))}");
+                    
+                }
+            }
+        }
+    }
+}
+
